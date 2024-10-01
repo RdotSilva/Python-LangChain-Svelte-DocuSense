670dddf12d0c6e2b64bb2afb13d45d3ec0b20227
RdotSilva <RyandotSilva@gmail.com>
2024-10-01 18:29:58 +0100

    Add basic chat model example

--- steaming_playground.py
+++ steaming_playground.py
@@ -1,1 +1,21 @@
 # Playground used to experiment with Streaming
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "{content}"),
+    ]
+)
+
+messages = prompt.format_messages(content="tell me a joke")
+
+output = chat(messages)
+
+print(output)
